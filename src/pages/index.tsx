fced24d85b784b53196df8dc9a010d66bd7c2d76
Minh Triet <bindsu63@gmail.com>
2023-03-16 11:44:44 +0700

    disable zoom in mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -124,7 +124,10 @@ export default function Home() {
       <Head>
         <title>Cards render</title>
         <meta name="description" content="Cards render | mtriet vjp pro" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+        />
         <meta name="referrer" content="no-referrer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
